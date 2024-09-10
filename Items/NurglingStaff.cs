cd7ce4848d00e7719d2befba94e5df0fcb669bce
Anthony <zmyantro@gmail.com>
2024-09-10 15:00:48 -0500

    Started Work on Minion

---
+++ Items/NurglingStaff.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using WarhammerLegendaryItems.Projectiles.Minions;
+using WarhammerLegendaryItems.Items.Placeable;
+using Microsoft.Xna.Framework;
+using WarhammerLegendaryItems.Buffs;
+
+
+namespace WarhammerLegendaryItems.Items
+{
+
+    public class NurglingStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true; // This lets the player target anywhere on the whole screen while using a controller
+            ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+
+            ItemID.Sets.StaffMinionSlotsRequired[Type] = 1f; // The default value is 1, but other values are supported. See the docs for more guidance. 
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 30;
+            Item.knockBack = 3f;
+            Item.mana = 10; // mana cost
+            Item.width = 32;
+            Item.height = 32;
+            Item.useTime = 36;
+            Item.useAnimation = 36;
+            Item.useStyle = ItemUseStyleID.Swing; // how the player's arm moves when using the item
+            Item.value = Item.sellPrice(gold: 30);
+            Item.rare = ItemRarityID.Cyan;
+            Item.UseSound = SoundID.Item44; // What sound should play when using the item
+
+            // These below are needed for a minion weapon
+            Item.noMelee = true; // this item doesn't do any melee damage
+            Item.DamageType = DamageClass.Summon; // Makes the damage register as summon. If your item does not have any damage type, it becomes true damage (which means that damage scalars will not affect it). Be sure to have a damage type
+            Item.buffType = ModContent.BuffType<NurglingMinionBuff>();
+            // No buffTime because otherwise the item tooltip would say something like "1 minute duration"
+            Item.shoot = ModContent.ProjectileType<NurglingMinion>(); // This item creates the minion projectile
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            // Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position
+            position = Main.MouseWorld;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            // This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
+            player.AddBuff(Item.buffType, 2);
+
+            // Minions have to be spawned manually, then have originalDamage assigned to the damage of the summon item
+            var projectile = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, Main.myPlayer);
+            projectile.originalDamage = Item.damage;
+
+            // Since we spawned the projectile manually already, we do not need the game to spawn it for ourselves anymore, so return false
+            return false;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<DemonSteelBar>(10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
